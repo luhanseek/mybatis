61bcd272e3b201ff1bf06b730fdbb51c6748a1c0
luhanseek <52238033+luhanseek@users.noreply.github.com>
2019-06-27 15:34:15 +0800

    Update MybatisApplicationTests.java

--- src/test/java/com/offcn/MybatisApplicationTests.java
+++ src/test/java/com/offcn/MybatisApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MybatisApplicationTests {
-
+//测试idea Git克隆
     @Test
     public void contextLoads() {
     }
